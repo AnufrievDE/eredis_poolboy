a75e4b34966efa8650056d251a8087c51eb5b3a4
Denis Anufriev <anufriev.denis.e@gmail.com>
2022-01-20 16:01:55 +0300

    Add env variables to configure pools at application startup

--- src/eredis_poolboy.erl
+++ src/eredis_poolboy.erl
@@ -1,85 +1,107 @@
-%%-------------------------------------------------------------------
-%% @doc
-%% This file is part of eredis + poolboy.
-%% @end
-%%-------------------------------------------------------------------
+%%%----------------------------------------------------------------------------
+%%% @doc
+%%% This file is part of eredis + poolboy.
+%%% @end
+%%%----------------------------------------------------------------------------
 
 -module(eredis_poolboy).
 
 %% API
 -export([
-  add_pool/3,
-  q/2,
-  q/3,
-  qp/2,
-  qp/3,
-  q_noreply/2
+    add_pool/2, add_pool/3, add_pool/4,
+    q/1, q/2, q/3,
+    qp/1, qp/2, qp/3,
+    q_noreply/1, q_noreply/2
 ]).
 
 -include_lib("eredis/include/eredis.hrl").
 
-%% @doc
-%% Add new pool connections.
-%% @end
--spec add_pool(PoolName, PoolSize, Option) -> {ok, pid} | {error, Reason} when
-  PoolName :: atom(),
-  PoolSize :: proplists:proplist(),
-  Option   :: option(),
-  Reason   :: {already_started, pid()}.
-add_pool(PoolName, PoolSize, Option) ->
-  eredis_poolboy_sup:add_pool(PoolName, PoolSize, Option).
+-define(DEFAULT_POOL, eredis_pool).
 
-%% @doc
-%% Executes the given command. The command must be a valid Redis command.
-%% @end
+%%%----------------------------------------------------------------------------
+%%% @doc
+%%% Add new pool connections.
+%%% @end
+%%%----------------------------------------------------------------------------
+-spec add_pool(PoolName, EredisAndPoolArgs) -> supervisor:startchild_ret() when
+    PoolName :: atom(),
+    EredisAndPoolArgs :: proplists:proplist().
+add_pool(PoolName, EredisAndPoolArgs) ->
+    eredis_poolboy_sup:add_pool(PoolName, EredisAndPoolArgs).
+
+-spec add_pool(PoolName, PoolArgs, EredisArgs) ->
+    supervisor:startchild_ret() when
+    PoolName :: atom(),
+    PoolArgs :: proplists:proplist(),
+    EredisArgs   :: options().
+add_pool(PoolName, PoolArgs, EredisArgs) ->
+    eredis_poolboy_sup:add_pool(PoolName, PoolArgs, EredisArgs).
+
+-spec add_pool(PoolName, PoolArgs, GlobalOrLocal, EredisArgs) ->
+    supervisor:startchild_ret() when
+    PoolName :: atom(),
+    PoolArgs :: proplists:proplist(),
+    GlobalOrLocal :: global | local,
+    EredisArgs   :: options().
+add_pool(PoolName, PoolArgs, GlobalOrLocal, EredisArgs) ->
+    eredis_poolboy_sup:add_pool(PoolName, PoolArgs, GlobalOrLocal, EredisArgs).
+
+%%%----------------------------------------------------------------------------
+%%% @doc
+%%% Executes the given command. The command must be a valid Redis command.
+%%% @end
+%%%----------------------------------------------------------------------------
+-spec q(Command :: [any()]) ->
+    {ok, return_value()} | {error, Reason :: binary() | no_connection}.
+q(Command) -> q(?DEFAULT_POOL, Command).
+
 -spec q(PoolName :: atom(), Command :: [any()]) ->
-  {ok, return_value()} | {error, Reason :: binary() | no_connection}.
+    {ok, return_value()} | {error, Reason :: binary() | no_connection}.
 q(PoolName, Command) ->
-  Func = fun(Worker) ->
-    eredis:q(Worker, Command)
-  end,
-  poolboy:transaction(PoolName, Func).
+    Fun = fun(Worker) -> eredis:q(Worker, Command) end,
+    poolboy:transaction(PoolName, Fun).
 
 -spec q(PoolName, Command, Timeout) ->
-  {ok, return_value()} | {error, Reason :: binary() | no_connection} when
-  PoolName :: atom(),
-  Command  :: [any()],
-  Timeout  :: non_neg_integer() | infinity.
+    {ok, return_value()} | {error, Reason :: binary() | no_connection} when
+    PoolName :: atom(),
+    Command  :: [any()],
+    Timeout  :: non_neg_integer() | infinity.
 q(PoolName, Command, Timeout) ->
-  Func = fun(Worker) ->
-    eredis:q(Worker, Command, Timeout)
-  end,
-  poolboy:transaction(PoolName, Func).
+    Fun = fun(Worker) -> eredis:q(Worker, Command, Timeout) end,
+    poolboy:transaction(PoolName, Fun).
 
-%% @doc
-%% Executes the given pipeline (list of commands) in the
-%% specified connection. The commands must be valid Redis commands.
-%% @end
--spec qp(PoolName :: atom(), Command :: [any()]) ->
-  {ok, return_value()} | {error, Reason :: binary() | no_connection}.
-qp(PoolName, Command) ->
-  Func = fun(Worker) ->
-    eredis:qp(Worker, Command)
-  end,
-  poolboy:transaction(PoolName, Func).
+%%%----------------------------------------------------------------------------
+%%% @doc
+%%% Executes the given pipeline (list of commands) in the
+%%% specified connection. The commands must be valid Redis commands.
+%%% @end
+%%%----------------------------------------------------------------------------
+-spec qp(pipeline()) ->
+    [{ok, return_value()} | {error, Reason::binary()}] | {error, no_connection}.
+qp(Pipeline) -> qp(?DEFAULT_POOL, Pipeline).
 
--spec qp(PoolName, Command, Timeout) ->
-  {ok, return_value()} | {error, Reason :: binary() | no_connection} when
-  PoolName :: atom(),
-  Command  :: [any()],
-  Timeout  :: non_neg_integer() | infinity.
-qp(PoolName, Command, Timeout) ->
-  Func = fun(Worker) ->
-    eredis:qp(Worker, Command, Timeout)
-  end,
-  poolboy:transaction(PoolName, Func).
+-spec qp(PoolName :: atom(), pipeline()) ->
+    [{ok, return_value()} | {error, Reason::binary()}] | {error, no_connection}.
+qp(PoolName, Pipeline) ->
+    Fun = fun(Worker) -> eredis:qp(Worker, Pipeline) end,
+    poolboy:transaction(PoolName, Fun).
 
-%% @doc
-%% Executes the command but does not wait for a response and ignores any errors.
-%% @end
+-spec qp(PoolName :: atom(), pipeline(), Timeout) ->
+    [{ok, return_value()} | {error, Reason::binary()}] | {error, no_connection}
+    when Timeout :: non_neg_integer() | infinity.
+qp(PoolName, Pipeline, Timeout) ->
+    Fun = fun(Worker) -> eredis:qp(Worker, Pipeline, Timeout) end,
+    poolboy:transaction(PoolName, Fun).
+
+%%%----------------------------------------------------------------------------
+%%% @doc
+%%% Executes the command but does not wait for a response and ignores any errors.
+%%% @end
+%%%----------------------------------------------------------------------------
+-spec q_noreply(Command :: [any()]) -> ok.
+q_noreply(Command) -> q_noreply(?DEFAULT_POOL, Command).
+
 -spec q_noreply(PoolName :: atom(), Command :: [any()]) -> ok.
 q_noreply(PoolName, Command) ->
-  Func = fun(Worker) ->
-    eredis:q_noreply(Worker, Command)
-  end,
-  poolboy:transaction(PoolName, Func).
+    Fun = fun(Worker) -> eredis:q_noreply(Worker, Command) end,
+    poolboy:transaction(PoolName, Fun).
--- src/eredis_poolboy_sup.erl
+++ src/eredis_poolboy_sup.erl
@@ -1,9 +1,10 @@
-%%-------------------------------------------------------------------
-%% @doc
-%% This file is part of eredis + poolboy.
-%% eredis_poolboy top level supervisor.
-%% @end
-%%-------------------------------------------------------------------
+
+%%%----------------------------------------------------------------------------
+%%% @doc
+%%% This file is part of eredis + poolboy.
+%%% eredis_poolboy top level supervisor.
+%%% @end
+%%%----------------------------------------------------------------------------
 
 -module(eredis_poolboy_sup).
 
@@ -11,48 +12,76 @@
 
 %% API
 -export([
-  start_link/0,
-  add_pool/3
+    start_link/0,
+    add_pool/2, add_pool/3, add_pool/4
 ]).
 
 %% Supervisor callbacks
--export([
-  init/1
-]).
+-export([init/1]).
 
 -include_lib("eredis/include/eredis.hrl").
 
-%%====================================================================
-%% API functions
-%%====================================================================
-
+%%%----------------------------------------------------------------------------
+%%% API functions
+%%%----------------------------------------------------------------------------
 -spec start_link() -> supervisor:startlink_ret().
 start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, application:get_all_env()).
 
-%% @doc
-%% Add new pool connections.
-%% @end
--spec add_pool(PoolName, PoolSize, Option) -> supervisor:startchild_ret() when
-  PoolName :: atom(),
-  PoolSize :: proplists:proplist(),
-  Option   :: option().
-add_pool(PoolName, PoolSize, Option) ->
-  PoolArgs = [{name, {local, PoolName}},
-    {worker_module, eredis}] ++ PoolSize,
-  PoolSpec = poolboy:child_spec(PoolName, PoolArgs, Option),
-  supervisor:start_child(?MODULE, PoolSpec).
+%%%----------------------------------------------------------------------------
+%%% @doc
+%%% Add new pool connections.
+%%% @end
+%%%----------------------------------------------------------------------------
+-spec add_pool(PoolName, EredisAndPoolArgs) -> supervisor:startchild_ret() when
+    PoolName :: atom(),
+    EredisAndPoolArgs :: proplists:proplist().
+add_pool(PoolName, EredisAndPoolArgs) ->
+    add_pool(PoolName, EredisAndPoolArgs, EredisAndPoolArgs).
 
-%%====================================================================
-%% Supervisor callbacks
-%%====================================================================
+-spec add_pool(PoolName, PoolArgs, EredisArgs) ->
+    supervisor:startchild_ret() when
+    PoolName :: atom(),
+    PoolArgs :: proplists:proplist(),
+    EredisArgs   :: options().
+add_pool(PoolName, PoolArgs, EredisArgs) ->
+    add_pool(PoolName, PoolArgs, local, EredisArgs).
 
-init([]) ->
-  SupFlags = #{
-    strategy => one_for_one
-  },
-  {ok, {SupFlags, []}}.
+-spec add_pool(PoolName, PoolArgs, GlobalOrLocal, EredisArgs) ->
+    supervisor:startchild_ret() when
+    PoolName :: atom(),
+    PoolArgs :: proplists:proplist(),
+    GlobalOrLocal :: global | local,
+    EredisArgs   :: options().
+add_pool(PoolName, PoolArgs, GlobalOrLocal, EredisArgs) ->
+    PoolArgs = pool_args(GlobalOrLocal, PoolName, EredisArgs),
+    PoolSpec = poolboy:child_spec(PoolName, PoolArgs, EredisArgs),
+    supervisor:start_child(?MODULE, PoolSpec).
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+%%%----------------------------------------------------------------------------
+%%% Supervisor callbacks
+%%%----------------------------------------------------------------------------
+init(Env) ->
+    GlobalOrLocal = proplists:get_value(global_or_local, Env),
+    Pools = proplists:get_value(pools, Env),
+    PoolSpecs =
+        lists:map(fun({PoolName, EredisAndPoolArgs}) ->
+            PoolArgs = pool_args(GlobalOrLocal, PoolName, EredisAndPoolArgs),
+            #{
+                id => PoolName,
+                start => {poolboy, start_link, [PoolArgs, EredisAndPoolArgs]}
+            }
+        end, Pools),
+
+    SupFlags = #{
+        strategy => one_for_one,
+        intensity => 2,
+        period => 5
+    },
+    {ok, {SupFlags, PoolSpecs}}.
+
+%%%----------------------------------------------------------------------------
+%%% Internal functions
+%%%----------------------------------------------------------------------------
+pool_args(GlobalOrLocal, PoolName, EredisArgs) ->
+    [{name, {GlobalOrLocal, PoolName}}, {worker_module, eredis} | EredisArgs].
